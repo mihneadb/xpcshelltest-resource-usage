d65717ab56b5f7247924eeaa5b448ddccfb6dab1
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-08-21 15:28:34 -0700

    upload file

--- js/plot.js
+++ js/plot.js
@@ -1,6 +1,7 @@
 var BAR_WIDTH = 10;
 var MIN_HEIGHT = 5;
 
+var data = null;
 var plotted = false;
 
 var getters = {
@@ -18,7 +19,9 @@ var getters = {
     }
 };
 
-function plotGraph() {
+function plotGraph(data) {
+    var f = $("#file");
+    console.log(f);
     // remove old chart and make new one
     $("#chart").remove();
     $("#chart-div").append('<svg id="chart"></svg>');
@@ -30,54 +33,67 @@ function plotGraph() {
     var sort = $("#sort")[0].checked;
     var getterFunc = getters[key];
 
-    d3.json("resources.json", function(data) {
-        data = data.filter(function(elem) {
-            return getterFunc(elem) !== null && getterFunc(elem) !== undefined;
-        });
-
-        tip = d3.tip().html(function(d) {
-            return "<div class='d3-tip'>" +
-                "<pre>" + JSON.stringify(d, null, '\t') + "</pre>" +
-                "</div>";
-        });
-        chart.call(tip);
-
-        if (sort) {
-            data.sort(function(a, b) {
-                var x = getterFunc(a);
-                var y = getterFunc(b);
-                return d3.descending(x, y);
-            });
-        }
-
-        var computeHeight = d3.scale.linear()
-        .domain([MIN_HEIGHT, d3.max(data, getterFunc)])
-        .range([MIN_HEIGHT, chartHeight]);
-
-        chart.attr("width", data.length * BAR_WIDTH)
-            .attr("height", chartHeight);
-        chart.selectAll("rect")
-            .data(data)
-            .enter().append("rect")
-            .attr("x", function(d, i) { return i * BAR_WIDTH; })
-            .attr("y", function(d, i) { return chartHeight - computeHeight(getterFunc(d)); })
-            .attr("height", function(d, i) { return computeHeight(getterFunc(d)); })
-            .attr("width", BAR_WIDTH)
-            .on('mouseover', function(d) {
-                tip.show(d);
-                // center it horizontally
-                var tipWidth = $('.d3-tip').width();
-                $('.d3-tip').css('left', $(window).width() / 2 - tipWidth / 2 + 'px');
-            })
-            .on('mouseout', tip.hide);
+    // plot it
+    data = data.filter(function(elem) {
+        return getterFunc(elem) !== null && getterFunc(elem) !== undefined;
     });
+
+    tip = d3.tip().html(function(d) {
+        return "<div class='d3-tip'>" +
+            "<pre>" + JSON.stringify(d, null, '\t') + "</pre>" +
+            "</div>";
+    });
+    chart.call(tip);
+
+    if (sort) {
+        data.sort(function(a, b) {
+            var x = getterFunc(a);
+            var y = getterFunc(b);
+            return d3.descending(x, y);
+        });
+    }
+
+    var computeHeight = d3.scale.linear()
+    .domain([MIN_HEIGHT, d3.max(data, getterFunc)])
+    .range([MIN_HEIGHT, chartHeight]);
+
+    chart.attr("width", data.length * BAR_WIDTH)
+        .attr("height", chartHeight);
+    chart.selectAll("rect")
+        .data(data)
+        .enter().append("rect")
+        .attr("x", function(d, i) { return i * BAR_WIDTH; })
+        .attr("y", function(d, i) { return chartHeight - computeHeight(getterFunc(d)); })
+        .attr("height", function(d, i) { return computeHeight(getterFunc(d)); })
+        .attr("width", BAR_WIDTH)
+        .on('mouseover', function(d) {
+            tip.show(d);
+            // center it horizontally
+            var tipWidth = $('.d3-tip').width();
+            $('.d3-tip').css('left', $(window).width() / 2 - tipWidth / 2 + 'px');
+        })
+        .on('mouseout', tip.hide);
     plotted = true;
 }
 
-window.onresize = function() {
+function handleFile(ev) {
+    var files = ev.target.files;
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+        data = JSON.parse(reader.result);
+        plotGraph(data);
+    };
+    reader.readAsText(file);
+}
+
+var replot = function() {
     if (plotted) {
-        plotGraph();
+        plotGraph(data);
     }
 };
+window.onresize = replot;
 
-$('#plot-btn').on('click', plotGraph);
+$('#file').on('change', handleFile);
+$('#key').on('change', replot);
+$('#sort').on('change', replot);
